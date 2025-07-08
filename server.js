264a9f418cca24d540bc5dc4964f32bc15a47e10
Jamie <jamiebuffing@gmail.com>
2025-07-08 08:30:58 +0200

    Push #1

---
+++ server.js
@@ -0,0 +1,29 @@
+console.log("Server start nu");
+
+const path = require('path');   //Basismodule uit Node gedefinieerd voor Multer 
+
+const express = require("express");
+const app = express();      
+
+const bcrypt = require('bcrypt');   // Voor de wachtwoorden om te Hashen
+
+const { MongoClient, ObjectId } = require("mongodb");
+
+const multer  = require('multer');
+
+const session = require("express-session");
+const { maxHeaderSize } = require('http');
+
+const storage = multer.diskStorage({    // De locatie van de upload afbeeldingen is in
+  destination: function (req, file, cb) {
+    cb(null, 'static/uploads/')         // de map static en dan uploads
+  },
+  filename: function (req, file, cb) {          // De afbeeldingen moeten volgens de volgende regels een naam krijgen.
+    const ext = path.extname(file.originalname);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) // Een random suffix string
+    cb(null, file.fieldname + '-' + uniqueSuffix + ext)     // De naam van het veld (Avatar) + de random Suffix en de bestands extensie
+  }
+})
+const upload = multer({ storage: storage }); // De opslag locatie van Multer is in de variabele storage.
+
+app.listen(3000)
